0171eb0e360f8082ddc186f83bc20eb0c200917c
Ofer Stolov <ofersto123@gmail.com>
2016-11-15 22:42:58 +0200

    Usused __init__

--- httpcat/middleware.py
+++ httpcat/middleware.py
@@ -4,9 +4,6 @@ from django.conf import settings
 OK_CODES = getattr(settings, "HTTP_OK_CODES", [200, 304])
 
 class HttpCatErrorHandler(object):
-    def __init__(self, get_response=None):
-        self.get_response = get_response
-
     def process_response(self, request, response):
         if response.status_code not in OK_CODES:
             url = "https://http.cat/" + str(response.status_code)
