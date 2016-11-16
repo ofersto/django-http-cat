5a9e43e613ef095c09d4e071952f5ab0984935c6
Ofer Stolov <ofersto123@gmail.com>
2016-11-16 07:34:10 +0200

    Pypi upload files

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name = 'django-http-cat',
+    packages = ['django-http-cat'],
+    version = '0.1',
+    description = 'Fetch pictures from http.cat as error pages',
+    license = "MIT",
+    author = 'Ofer Stolov',
+    author_email = 'ofersto123@gmail.com',
+    url = 'https://github.com/ofersto/django-http-cat',
+    download_url = 'https://github.com/ofersto/django-http-cat/tarball/0.1',
+    keywords = ['django', 'error handling', 'middleware', 'http.cat', 'cat'],
+    install_requires = [
+        "django"
+    ],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Framework :: Django",
+    ],
+)
